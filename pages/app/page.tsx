b11d851dfb610509492ad7ba9fd2950cbd91bccd
hocgin <hocgin@gmail.com>
2024-05-01 21:43:08 +0800

    dev

--- pages/app/page.tsx
+++ pages/app/page.tsx
@@ -1,13 +1,9 @@
-'use client';
-
 import {Mail} from "@/components/mail/mail"
 
 export default function Home() {
-  return (
-    <>
-      <div>
-        <Mail navCollapsedSize={4} />
-      </div>
-    </>
-  )
+  return <>
+    <div>
+      <Mail navCollapsedSize={4} />
+    </div>
+  </>
 }
