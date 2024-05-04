43cd756d12e99358ffb94b24c2c4f4d24b07985d
hocgin <hocgin@gmail.com>
2024-05-04 23:06:59 +0800

    dev

--- pages/service/http/app.ts
+++ pages/service/http/app.ts
@@ -5,28 +5,28 @@ import {IScroll} from "@hocgin/nextjs-kit";
 export class AppService {
 
   static listAccounts(): Promise<ListAccountVo> {
-    return useGet(`/api/user/account`)
+    return useGet(`/api/user/account`, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
       .then(RabbitKit.thenDataTryErrorIfExits);
   }
 
   static scrollByMail(payload: ChatUserScrollRo) {
     if (!payload?.owner) return Promise.any({list: [], hasMore: false, nextId: null} as any);
-    return usePost(`/api/mail/scroll`, {data: {...payload}})
+    return usePost(`/api/mail/scroll`, {data: {...payload}, headers: {'X-Requested-With': 'XMLHttpRequest'}})
       .then(RabbitKit.thenDataTryErrorIfExits).then(RabbitKit.getScrollData);
   }
 
   static scrollByHistory(payload: ChatHistoryScrollRo) {
-    return usePost(`/api/history/scroll`, {data: {...payload}})
+    return usePost(`/api/history/scroll`, {data: {...payload}, headers: {'X-Requested-With': 'XMLHttpRequest'}})
       .then(RabbitKit.thenDataTryErrorIfExits).then(RabbitKit.getScrollData);
   }
 
   static sendMail(ro: SendMailRo) {
-    return usePost(`/api/mail/send`, {data: {...ro}})
+    return usePost(`/api/mail/send`, {data: {...ro}, headers: {'X-Requested-With': 'XMLHttpRequest'}})
       .then(RabbitKit.thenDataTryErrorIfExits);
   }
 
   static getMail(id: number | string) {
-    return useGet(`/api/mail/${id}`)
+    return useGet(`/api/mail/${id}`, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
       .then(RabbitKit.thenDataTryErrorIfExits);
   }
 
